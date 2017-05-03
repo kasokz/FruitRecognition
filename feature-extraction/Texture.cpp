8faba9cf7f5f3ae5b6887325e8c25d1c57f75a07
Nord <ipuzyrev@gmail.com>
2017-05-03 13:21:33 +0200

    short to do plan

--- feature-extraction/Texture.cpp
+++ feature-extraction/Texture.cpp
@@ -2,3 +2,9 @@
 // Created by Long Bui on 25.04.17.
 //
 
+/*
+ * fill the 256-sized histogram array:
+ * 1. For every cell go the whole image through (polnish method)
+ * 2. or go the image only 1 time and fill the h-array by increments.
+ * 3. compare
+ */
